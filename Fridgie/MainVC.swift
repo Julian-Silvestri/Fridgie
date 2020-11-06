61944b874ae542733ba3e947b07d3d237d342dca
Julian Silvestri <juliansilvestri92@gmail.com>
2020-11-05 23:21:17 -0500

    saving work

--- Fridgie/MainVC.swift
+++ Fridgie/MainVC.swift
@@ -19,30 +19,54 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var emptyFridgeOpen: UIImageView!
     @IBOutlet weak var closedFridge: UIImageView!
     @IBOutlet weak var fridgeTV: UITableView!
+    @IBOutlet weak var titleLabel: UILabel!
     
     var fakeContents = ["Eggs","Bacon","Cheese","Milk","Bread","Olives","Chicken","Lettuce","Tomatoes","Cucumber","Zuchini","Peppers"]
     var groups = ["Protien","Protien","Dairy","Dairy","Grain","Fruit","Protien","Vegtable","Vegtable","Vegtable","Vegtable","Vegtable"]
     
+    var tapGesture = UITapGestureRecognizer()
     var currentContents = [String]()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         self.closedFridge.alpha = 1
         self.currentContents = fakeContents
+        self.fridgeTV.delegate = self
+        self.fridgeTV.dataSource = self
+        self.view.addGestureRecognizer(tapGesture)
+        self.tapGesture.addTarget(self, action: #selector(tapOccured))
+        if traitCollection.userInterfaceStyle == .dark{
+            self.view.backgroundColor = UIColor.white
+            self.titleLabel.textColor = UIColor.black
+            self.tabBarController?.view.tintColor = UIColor.black
+            self.tabBarController?.tabBar.barTintColor = UIColor.white
+            self.fridgeTV.backgroundColor = UIColor.white
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        
         super.viewWillAppear(animated)
+        self.fridgeTV.alpha = 0
+//        self.fullFridgeOpen.alpha = 0
+//        self.emptyFridgeOpen.alpha = 0
+        self.closedFridge.alpha = 1
         
-        if self.currentContents.count <= 0 {
-            
-        } else {
-            
-        }
-        
+        print(currentContents)
     }
+    
+    @objc func tapOccured(){
+        print("TAP OCCURED")
+        if self.fridgeTV.alpha == 0 {
+            print("opening Fridge")
+            self.openFridge()
+        } else {
+            print("Closing Fridge")
+            self.closeFridge()
+        }
+    }
+
     @IBAction func dontTouch(_ sender: Any) {
         if self.currentContents.count >= 1 {
             self.currentContents.removeAll()
@@ -57,18 +81,27 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = fridgeTV.dequeueReusableCell(withIdentifier: "fridgeCell") as! FridgeCell
         
+//        for data in currentContents[indexPath.row]{
+//            cell.name.text = data
+//        }
+//        for data in groups[indexPath.row] {
+//            cell.group.text = data
+//        }
+//
         cell.group.text = groups[indexPath.row]
-        cell.name.text = fakeContents[indexPath.row]
-        
+        cell.name.text = currentContents[indexPath.row]
+
         return cell
     }
     
     func closeFridge(){
         UIView.animate(withDuration: 0.5, animations: {
-            self.emptyFridgeOpen.alpha = 0
-            self.fullFridgeOpen.alpha = 0
+            self.fridgeTV.alpha = 0
         }, completion: { _ in
+//            self.emptyFridgeOpen.alpha = 0
+//            self.fullFridgeOpen.alpha = 0
             self.closedFridge.alpha = 1
+            return
         })
     }
     
@@ -76,17 +109,21 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         if self.closedFridge.alpha == 1 {
             if self.currentContents.count >= 1 {
                 UIView.animate(withDuration: 0.5, animations: {
-                    self.closedFridge.alpha = 0
-                    self.emptyFridgeOpen.alpha = 0
+                    self.closedFridge.alpha = 1
+//                    self.emptyFridgeOpen.alpha = 0
+//                    self.fullFridgeOpen.alpha = 1
                 },completion: { _ in
-                    self.fullFridgeOpen.alpha = 1
+                    self.fridgeTV.alpha = 1
+                    return
                 })
             } else {
                 UIView.animate(withDuration: 0.5, animations: {
                     self.closedFridge.alpha = 0
-                    self.fullFridgeOpen.alpha = 0
-                },completion: { _ in
+//                    self.fullFridgeOpen.alpha = 0
                     self.emptyFridgeOpen.alpha = 1
+                },completion: { _ in
+                    self.fridgeTV.alpha = 1
+                    return
                 })
             }
         }
